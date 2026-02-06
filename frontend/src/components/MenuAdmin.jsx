eeb32bc83e96670685d4ba49fe7a89061a970c40
josmar299 <a3522110243@alumno.uttehuacan.edu.mx>
2026-02-06 02:40:44 -0600

    Update MenuAdmin.jsx
    Removed unused refs for button and menu.

--- frontend/src/components/MenuAdmin.jsx
+++ frontend/src/components/MenuAdmin.jsx
@@ -5,9 +5,6 @@ export default function MenuAdmin() {
   const [mostrarMenu, setMostrarMenu] = useState(false);
   const navigate = useNavigate();
 
-   const botonRef = useRef(null);
-  const menuRef = useRef(null);
-
   const cerrarSesion = () => {
     localStorage.removeItem("admin");
     navigate("/admin");
