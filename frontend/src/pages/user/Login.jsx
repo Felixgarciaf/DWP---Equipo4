8b838f408086959e53f8568ad9cd7e26be3f096c
josmar299 <a3522110243@alumno.uttehuacan.edu.mx>
2026-02-08 00:46:18 -0600

    Update Login.jsx

--- frontend/src/pages/user/Login.jsx
+++ frontend/src/pages/user/Login.jsx
@@ -1,31 +1,47 @@
 import { useNavigate } from "react-router-dom";
+import "../../styles/styles.css";
 
 export default function Login() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>Iniciar Sesión</h1>
+    <div className="login-page">
+      <button className="btn-inicio" onClick={() => navigate("/")}>
+        Inicio
+      </button>
 
-      <div>
-        <label>Usuario o correo</label>
-        <br />
-        <input type="text" />
+      <div className="login-card">
+     
+        <div className="login-header">
+          <h2>Inicio de sesión</h2>
+        </div>
+
+        <div className="form-group">
+          <label>Usuario:</label>
+          <input type="text" />
+        </div>
+
+        <div className="form-group">
+          <label>Contraseña:</label>
+          <input type="password" />
+        </div>
+
+        <div className="login-actions">
+          <button
+            className="btn-secondary"
+            onClick={() => navigate("/register")}
+          >
+            Registrarse
+          </button>
+
+          <button
+            className="btn-primary"
+            onClick={() => navigate("/home")}
+          >
+            Iniciar sesión
+          </button>
+        </div>
       </div>
-
-      <br />
-
-      <div>
-        <label>Contraseña</label>
-        <br />
-        <input type="password" />
-      </div>
-
-      <br />
-
-      <button onClick={() => navigate("/home")}>Iniciar sesión</button>
-
-      <button onClick={() => navigate("/register")}>Registrarse</button>
     </div>
   );
 }
