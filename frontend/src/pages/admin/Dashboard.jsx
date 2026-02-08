1a4e0e060c6827c1fdceb4607003015b0ed0a9d0
josmar299 <a3522110243@alumno.uttehuacan.edu.mx>
2026-02-07 22:59:01 -0600

    Update Dashboard.jsx

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,36 +1,35 @@
+import "../../styles/styles.css";
+
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Panel de Control</h1>
+    <div className="admin-dashboard-page">
+     
+      <h1 className="admin-dashboard-title">Panel de Control</h1>
 
-      <p>Bienvenido al panel de administrador.</p>
-
-      <div style={{ display: "flex", gap: "20px", marginTop: "30px" }}>
-        <div
-          style={{
-            padding: "20px",
-            border: "1px solid #ccc",
-            borderRadius: "8px",
-            width: "250px",
-          }}
-        >
-          <h3>Total de reportes activos</h3>
-          <strong style={{ fontSize: "28px" }}>70</strong>
+      
+      <div className="admin-dashboard-cards">
+      
+        <div className="admin-card">
+          <div className="report-circle">
+            <span>70</span>
+          </div>
+          <p className="card-text">Total de reportes activos</p>
         </div>
 
-        <div
-          style={{
-            padding: "20px",
-            border: "1px solid #ccc",
-            borderRadius: "8px",
-            width: "350px",
-          }}
-        >
-          <h3>Últimos avisos publicados</h3>
-          <ul>
-            <li>Lomas de la Soledad – Fuga no atendida</li>
-            <li>Sardán – Reportes de fugas en red</li>
-            <li>Santiago Tula – Escasez y problemas de suministro</li>
+   
+        <div className="admin-card">
+          <h3 className="card-title">Últimos avisos publicados</h3>
+          <ul className="alerts-list">
+            <li>
+              <strong>Lomas de la Soledad</strong> – Fuga no atendida
+            </li>
+            <li>
+              <strong>Sedrán</strong> – Reportes de fugas en red
+            </li>
+            <li>
+              <strong>Santiago Tula</strong> – Escasez y problemas de
+              suministro
+            </li>
           </ul>
         </div>
       </div>
