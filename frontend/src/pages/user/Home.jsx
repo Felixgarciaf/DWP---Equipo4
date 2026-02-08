b564777c82f7ff5a7f5dff8a6559cbc4a98c4238
josmar299 <a3522110243@alumno.uttehuacan.edu.mx>
2026-02-08 00:47:49 -0600

    Update Home.jsx

--- frontend/src/pages/user/Home.jsx
+++ frontend/src/pages/user/Home.jsx
@@ -1,9 +1,29 @@
+import "../../styles/styles.css";
+
 export default function Home() {
   return (
-    <div>
-      <div>
-        <h1>Inicio </h1>
-      </div>
+    <div className="home-page">
+    
+      <section className="home-hero">
+        <div className="home-hero-content">
+          <div className="home-left">
+            <div className="home-title-skeleton"></div>
+            <div className="home-line-skeleton"></div>
+            <div className="home-line-skeleton short"></div>
+
+            <button className="home-btn">Iniciar sesión</button>
+          </div>
+
+          <div className="home-right">
+            <div className="home-image-placeholder"></div>
+          </div>
+        </div>
+      </section>
+
+      <section className="home-section">
+        <div className="home-section-title"></div>
+        <div className="home-section-content"></div>
+      </section>
     </div>
   );
 }
