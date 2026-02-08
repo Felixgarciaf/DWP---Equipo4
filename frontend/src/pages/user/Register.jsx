54cca528c591870abf91432d6a3d7b10bef9661a
josmar299 <a3522110243@alumno.uttehuacan.edu.mx>
2026-02-08 00:47:13 -0600

    Update Register.jsx

--- frontend/src/pages/user/Register.jsx
+++ frontend/src/pages/user/Register.jsx
@@ -1,17 +1,70 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../../styles/styles.css";
+
 export default function Register() {
+  const navigate = useNavigate();
+  const [showPassword,] = useState(false);
+
   return (
-    <div>
-      <h1>Registro Usuario</h1>
+    <div className="register-page">
+     
+      <button className="btn-inicio" onClick={() => navigate("/")}>
+        Inicio
+      </button>
 
-      <input placeholder="Nombre" />
-      <br />
-      <input placeholder="Correo" />
-      <br />
-      <input placeholder="Contraseña" type="password" />
-      <br />
+      <div className="register-card">
+        
+        <div className="register-header">
+          <h2>Registro</h2>
+        </div>
 
-      <button onClick={() => navigate("/Login")}>Volver Login</button>
-      <button onClick={() => navigate("/home")}>Crear Cuenta</button>
+        <div className="form-group">
+          <label>Nombre:</label>
+          <input type="text" />
+        </div>
+
+        <div className="form-group">
+          <label>Correo electrónico:</label>
+          <input type="email" />
+        </div>
+
+        <div className="form-group">
+          <label>Dirección:</label>
+          <input type="text" />
+        </div>
+
+        <div className="form-group">
+          <label>Medidor:</label>
+          <input type="text" />
+        </div>
+
+        <div className="form-row">
+          <div className="form-group">
+            <label>Contraseña:</label>
+            <input type={showPassword ? "text" : "password"} />
+          </div>
+
+          <div className="form-group">
+            <label>Confirmar Contraseña:</label>
+            <input type={showPassword ? "text" : "password"} />
+          </div>
+        </div>
+
+
+        <div className="register-footer">
+          <span className="login-link">
+            ¿Ya tienes una cuenta?
+          </span>
+
+          <button
+            className="btn-register"
+            onClick={() => navigate("/home")}
+          >
+            Registrarte
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
